06c6e2446d0d74e2f64d16c6520e11b327670bc2
rudranil08 <94041938+rudranil08@users.noreply.github.com>
2023-11-18 20:57:04 +0530

    Weather app Updated v 1.0

--- getWeather.js
+++ getWeather.js
@@ -1,10 +1,9 @@
 async function getWeather() {
   const apiKey = 'aa74796b476012b61a3c231e2eccf2c7';
   const city = document.getElementById('cityInput').value;
-  const locationElement = document.getElementById('location');
   const temperatureElement = document.getElementById('temperature');
   const descriptionElement = document.getElementById('description');
-  const humidityElement = document.getElementById('Humidity');
+
   
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
@@ -17,14 +16,13 @@ async function getWeather() {
     }
 
     const data = await response.json();
-    locationElement.textContent = data.name;
     temperatureElement.textContent = data.main.temp;
     descriptionElement.textContent = data.weather[0].description;
   } catch (error) {
     console.log('Error fetching weather data:', error);
-    locationElement.textContent = '';
     temperatureElement.textContent = '';
-    descriptionElement.textContent = 'City not found';
+    descriptionElement.textContent = '';
+
   }
-  
+
 }
